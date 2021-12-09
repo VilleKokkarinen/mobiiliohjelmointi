3012c949404550d578a81b6e7a72d3f411bd3b8c
Teemu991 <33228942+Teemu991@users.noreply.github.com>
2021-12-09 19:34:19 +0200

    Logout lisätty

--- app/src/main/java/com/example/login/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/login/ui/home/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.login.ui.home
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,9 +10,13 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.login.R
 import com.example.login.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
 
     private lateinit var binding: ActivityMainBinding
+
+    constructor(parcel: Parcel) : this() {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +29,23 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_fragment)
         bottomNavigationView.setupWithNavController(navController
         )
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
     }
 }
